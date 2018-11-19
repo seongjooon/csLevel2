c6395c8c3c34dbb7d6a4316ca75fb96ae8082c03
Peter Kim <seongjooon@gmail.com>
2018-11-19 21:18:47 +0900

    f

--- firstTime/greatestCommonMeasure2.js
+++ firstTime/greatestCommonMeasure2.js
@@ -9,18 +9,18 @@ function getGreatestCommonMeasure(userNumbers) {
     var greatestCommonMeasure = 0;
     if (userNumbers[0] > userNumbers[1]) {
         for (i = 1; i <= userNumbers[1]; i++) {
-            if (userNumbers[0] % i == 0 && userNumbers[1] % i == 0) {
+            if (userNumbers[0] % i == 0 && userNumbers[1] %i ==0) {
                 greatestCommonMeasure = i;
             }
         }
     } else if (userNumbers[0] < userNumbers[1]) {
         for (i = 1; i <= userNumbers[0]; i++) {
-            if (userNumbers[0] % i == 0 && userNumbers[1] % i == 0) {
+            if (userNumbers[0] % i == 0 && userNumbers[1] %i ==0) {
                 greatestCommonMeasure = i;
             }
         }
     } else {
-        greatestCommonMeasure = userNumbers[0];
+        greatestCommonMeasure = userNumbers[0] ;
     }
     return greatestCommonMeasure;
 }
